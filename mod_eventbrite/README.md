e9cc7780731a98916aab24943eacb72e0e2f473d
David Hendy <david.hendy@2buy2.com>
2021-05-04 16:46:30 +0100

    Finessing of folder structure

--- mod_eventbrite/README.md
+++ mod_eventbrite/README.md
@@ -1,4 +1,4 @@
-# Mod_Eventbrite
+# Mod_Eventbrite v.0.0.2
 
 ## What is it used for?
 This module will be used for displaying events from Eventbrite in Joomla. 
--- mod_eventbrite/helper.php
+++ mod_eventbrite/helper.php
@@ -22,9 +22,41 @@ use Joomla\CMS\Response\JsonResponse;
 
 class modEventbriteHelper{
 
+	private $base_url = 'https://www.eventbriteapi.com/v3/';
+
 	public function getParams($params, $type)
 	{
 		return $params->get($type);
+	}
+
+	private function restCall($url)
+	{
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$response = curl_exec($ch);
+		$err = curl_error($ch);
+		curl_close($ch);
+		if($err){
+			Log::add($err, Log::DEBUG, 'mod-eventbrite');
+			return false;
+		} else {
+			if($response){
+				return json_decode($response);
+			} else {
+				Log::add('No valid response given', Log::DEBUG, 'mod-eventbrite');
+				return false;
+			}
+		}
+	}
+
+	public function getEvents(string $org_id = '', string $apikey = '')
+	{
+		$url = $this->base_url.'organizations/'.$org_id.'/events/?expand=venue,organizer&status=live&token='.$apikey;
+		return $this->restCall($url);
 	}
 
 }
